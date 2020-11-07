cc223670587c4f235ad8892f616180c2dc15c72b
majkic99 <60391069+majkic99@users.noreply.github.com>
2020-11-07 23:23:37 +0100

    push

--- StorageComp-API/src/main/java/api/StorageSpec.java
+++ StorageComp-API/src/main/java/api/StorageSpec.java
@@ -3,22 +3,32 @@ package api;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import model.Entity;
 
 public abstract class StorageSpec {
 	
 	protected String folderName;
-	protected int cap = 50;
+	protected final int cap = 50;
 	protected String currFileName;
 	
-	private boolean isAutoIncrement; // TODO mogucnost izbora
+	private boolean isAutoIncrement;
 	
+<<<<<<< Updated upstream
 	public abstract void runDB(String fileName) throws Exception; //unutar implementacije proveriti da li se file zavrsava (.xxxx) sa dobrim nastavkom 
 	//
+=======
+	public abstract void runDB(String fileName) throws Exception; 
+>>>>>>> Stashed changes
 	
 	public abstract void save(Entity entity) throws IOException;
 	
@@ -28,7 +38,7 @@ public abstract class StorageSpec {
 		
 		if(isAutoIncrement) {
 			System.out.println("Id je ignorisan(Autoincrement)");
-			Entity entity = new Entity(name, properties, subEntities); // properties ili subEntities moze da bude null (handluje svaki slucaj)
+			Entity entity = new Entity(name, properties, subEntities); 
 			save(entity);
 			
 		} else {
@@ -69,22 +79,92 @@ public abstract class StorageSpec {
 		return listaRezultata;
 	}
 	
-	public List<Entity> findSpecial(String string){
+	public List<Entity> findByKeyValue(String key, String value) throws Exception{
+		List<Entity> lista = readAll();
 		List<Entity> listaRezultata = new ArrayList<Entity>();
-				//TODO
+		for (Entity en : lista) {
+			if (en.getProperties().get(key).equals(value)) {
+				listaRezultata.add(en);
+			}
+		}
+		
 		return listaRezultata;
 	}
 	
-	public List<Entity> sortSpecial(String string){
+	public List<Entity> findSpecial(String string) throws Exception{
+		
+		List<Entity> lista = readAll();
+		
+		List<String> tokens = new ArrayList<>(Arrays.asList(string.split(" and ")));
+		String[][] doubleTokens = new String[1000][1000];
+		
+		for (int i = 0; i < tokens.size(); i++) {
+			doubleTokens[i] = tokens.get(i).split("=");
+		}
+		
+		for (int j = 0; j < doubleTokens.length; j++) {
+			for (Entity en : lista) {
+				if (!(en.getProperties().get(doubleTokens[j][0]).startsWith(doubleTokens[j][1]))) {
+					lista.remove(en);
+				}
+			}
+		}
+		
+		return lista;
+	}
+	
+	public List<Entity> sortSpecial(String key, Boolean rising) throws Exception{
 		List<Entity> listaRezultata = new ArrayList<Entity>();
-		//TODO
+		List<Entity> lista = readAll();
+		Map<Integer, String> mapa = new HashMap<Integer, String>();
+		
+		for (Entity ent : lista) {
+			if (ent.getProperties().get(key) == null) {
+				lista.remove(ent);
+			}
+		}
+		
+		for (Entity en : lista) {
+			mapa.put(Integer.valueOf(en.getId()),en.getProperties().get(key));
+		}
+		
+		Map<Integer, String> sortedMap = 
+			     mapa.entrySet().stream()
+			    .sorted(Entry.comparingByValue())
+			    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+			                              (e1, e2) -> e1, LinkedHashMap::new));
+		
+		for(Entry<Integer, String> entry : sortedMap.entrySet()) {
+		    Integer id = entry.getKey();
+		    String value = entry.getValue();
+		    listaRezultata.add(read(id));
+		}
+		
 		return listaRezultata;
 	}
 	
 	public void deleteSpecial(String string) throws Exception{
 		List<Integer> listaRezultata = new ArrayList<Integer>();
 		List<Entity> lista = readAll();
-		//TODO
+		
+		List<String> tokens = new ArrayList<>(Arrays.asList(string.split(" and ")));
+		String[][] doubleTokens = new String[1000][1000];
+		
+		for (int i = 0; i < tokens.size(); i++) {
+			doubleTokens[i] = tokens.get(i).split("=");
+		}
+		
+		for (int j = 0; j < doubleTokens.length; j++) {
+			for (Entity en : lista) {
+				if (!(en.getProperties().get(doubleTokens[j][0]).startsWith(doubleTokens[j][1]))) {
+					lista.remove(en);
+				}
+			}
+		}
+		
+		for (Entity ent : lista) {
+			listaRezultata.add(ent.getId());
+		}
 		
 		for (Integer i : listaRezultata) {
 			delete(i);
@@ -96,7 +176,7 @@ public abstract class StorageSpec {
 	
 	public abstract void delete(int id) throws IOException;
 	
-	public void loadUsedIDs() {
+	protected void loadUsedIDs() {
 		//citanje iz svih fajlova unutar foldera
 		
 		try {
@@ -145,9 +225,7 @@ public abstract class StorageSpec {
 		return cap;
 	}
 
-	public void setCap(int cap) {
-		this.cap = cap;
-	}
+
 
 	public String getCurrFileName() {
 		return currFileName;
@@ -157,7 +235,6 @@ public abstract class StorageSpec {
 		this.currFileName = currFileName;
 	}
 
-	
-	
+
 	
 }

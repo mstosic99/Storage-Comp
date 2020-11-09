84300a17fcbd5ba7497f7dbb2366e8a32cd37709
majkic99 <60391069+majkic99@users.noreply.github.com>
2020-11-09 13:38:17 +0100

    dokumentacija odradjena

--- StorageComp-API/src/main/java/api/ImplementorManager.java
+++ StorageComp-API/src/main/java/api/ImplementorManager.java
@@ -8,7 +8,10 @@ public class ImplementorManager {
 	public static void registerImplementor(StorageSpec spec) {
 		storageSpec = spec;	
 	}
-	
+	/**
+	* Runtime dependency start
+	* @return returns an instance of StorageSpec depending on your Implementation
+	*/
 	public static StorageSpec getStorageSpec() {
 		return storageSpec;
 	}
--- StorageComp-API/src/main/java/api/StorageSpec.java
+++ StorageComp-API/src/main/java/api/StorageSpec.java
@@ -23,14 +23,31 @@ public abstract class StorageSpec {
 	
 	private boolean isAutoIncrement;
 	
+	/**
+	* Sets the current folder that the StorageSpec works with, and checks if the folder matches the Implementation version.
+	* Exits the program if it does not.
+	* @param folderName folder Name in String format (absolute path).
+	*/
+	public abstract void runDB(String folderName) throws Exception; 
 
-	public abstract void runDB(String fileName) throws Exception; 
-
-	
+	/**
+	* Saves one Entity in database.
+	* @param entity Entity that you want to be saved.
+	*/
 	public abstract void save(Entity entity) throws IOException;
-	
+	/**
+	* Saves a list of Entity in database.
+	* @param entities List of Entity that you want to be saved.
+	*/
 	public abstract void save(List<Entity> entities);
 	
+	/**
+	* Creates an entity out of paramateres and saves it to DB.
+	* @param id ID of Entity that you want to be saved.
+	* @param name Name of Entity that you want to be saved.
+	* @param properties Properties of Entity that you want to be saved.
+	* @param subEntities subEntities of Entity that you want to be saved.
+	*/
 	public void save(int id, String name, Map<String, String> properties, Map<String, List<Entity>> subEntities) throws IOException {
 		
 		if(isAutoIncrement) {
@@ -45,18 +62,41 @@ public abstract class StorageSpec {
 		
 		
 	}
-	
+	/**
+	* Reads all Entity in database.
+	* @return returns a list of Entities that was read.
+	*/
 	public abstract List<Entity> readAll() throws IOException;				// Ucitaj sve entitete
 	
+	/**
+	* Reads one Entity in database.
+	* @param  id Entity id that you want to be read.
+	* @return returns an Entity that was read.
+	*/
 	public abstract Entity read(int id) throws IOException;
 	
+	/**
+	* Reads one Entity in database.
+	* @param  ids Entity id list that you want to be read.
+	* @return returns a list of Entities that was read.
+	*/
 	public abstract List<Entity> read(int[] ids) throws IOException;  		// Ucitaj entitete za id
 	
+	/**
+	* Updates the entity in the DB
+	* @param  entity Entity id list that you want to be read.
+	*/
 	public void update(Entity entity) throws Exception {
 		delete(entity.getId());
 		save(entity);
 	}
 	
+	/**
+	* Adds a subentity to an Entity that you know ID of
+	* @param  id Entity id list that you want to be read.
+	* @param  subEntity Subentity that you want to put.
+	* @param  key key for that subentity.
+	*/
 	public void addSubEntityToEntity(int id, Entity subEntity, String key) throws Exception {
 		Entity entity = read(id);
 		List<Entity> value = new ArrayList<Entity>();
@@ -65,6 +105,11 @@ public abstract class StorageSpec {
 		update(entity);
 	}
 	
+	/**
+	* Returns a list of Entity that have that name.
+	* @param  name Entity name that you want to find.
+	* @return returns a List of all Entity objects that have that name in DB
+	*/
 	public List<Entity> findByName(String name) throws Exception{
 		List<Entity> lista = readAll();
 		List<Entity> listaRezultata = new ArrayList<Entity>();
@@ -76,7 +121,12 @@ public abstract class StorageSpec {
 		
 		return listaRezultata;
 	}
-	
+	/**
+	* Returns a list of Entity with that key value pair
+	* @param  key Entity name that you want to find.
+	* @param  value Entity name that you want to find.
+	* @return List of all Entity objects that have that key:value combo in DB
+	*/
 	public List<Entity> findByKeyValue(String key, String value) throws Exception{
 		List<Entity> lista = readAll();
 		List<Entity> listaRezultata = new ArrayList<Entity>();
@@ -89,6 +139,11 @@ public abstract class StorageSpec {
 		return listaRezultata;
 	}
 	
+	/**
+	* Returns a list of Entity that were found. The format for this is key=value (and key=value)
+	* @param  string Entity keyvalue pairs that you want to find.
+	* @return List of all Entity objects that have that key:value combo in DB
+	*/
 	public List<Entity> findSpecial(String string) throws Exception{
 		
 		List<Entity> lista = readAll();
@@ -116,6 +171,12 @@ public abstract class StorageSpec {
 		return lista;
 	}
 	
+	/**
+	* Returns a sorted list of Entity that were found containing that key.
+	* @param  key Key by which you are sorting
+	* @param  rising True for rising, False for falling order
+	* @return Sorted list of entities containing that key
+	*/
 	public List<Entity> sortSpecial(String key, Boolean rising) throws Exception{
 		List<Entity> listaRezultata = new ArrayList<Entity>();
 		List<Entity> lista = readAll();
@@ -145,9 +206,15 @@ public abstract class StorageSpec {
 		    listaRezultata.add(read(id));
 		}
 		
+		if (!(rising)) {
+			Collections.reverse(listaRezultata);
+		}
 		return listaRezultata;
 	}
-	
+	/**
+	* Deletes all Entity that were found. The format for this is key=value (and key=value)
+	* @param  string Entity keyvalue pairs that you want to delete.
+	*/
 	public void deleteSpecial(String string) throws Exception{
 		List<Integer> listaRezultata = new ArrayList<Integer>();
 		List<Entity> lista = readAll();
@@ -178,9 +245,15 @@ public abstract class StorageSpec {
 		}
 	}
 	
-	
+	/**
+	* Deletes all Entity that were found with these IDs
+	* @param  ids List of IDs that you want deleted
+	*/
 	public abstract void delete(int[] ids) throws IOException;
-	
+	/**
+	* Deletesa all Entity that were found with this ID.
+	* @param  id ID by which to delete
+	*/
 	public abstract void delete(int id) throws IOException;
 	
 	protected void loadUsedIDs() {
@@ -210,7 +283,10 @@ public abstract class StorageSpec {
 		}
 	}
 	
-	
+	/**
+	* Starts the database for this folder
+	* @param folderName absolutePath of folder
+	*/
 	public void setFolderNameAndStart(String folderName) throws Exception {
 		this.folderName = folderName;
 		runDB(folderName);

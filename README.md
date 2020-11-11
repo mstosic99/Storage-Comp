60df24bdb6a61f9163c7b6cd8ec1976d2b1543af
majkic99 <60391069+majkic99@users.noreply.github.com>
2020-11-11 16:00:46 +0100

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Storage-Comp
 Java component for storing json, xml, yaml, and custom format files.
+
+-Odkomentarisati jedan dependensi koji je potreban u pom.xml fajlu i izbildovati.
--- StorageComp-CustomImpl/src/main/java/impl/CustomStorageImplementation.java
+++ StorageComp-CustomImpl/src/main/java/impl/CustomStorageImplementation.java
@@ -12,8 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 import api.ImplementorManager;
 import api.StorageSpec;
